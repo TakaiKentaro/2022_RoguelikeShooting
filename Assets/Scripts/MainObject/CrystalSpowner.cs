a29a8b559bf1a328a310b1506588d4de439aa3dd
TakaiKentarou <mmpm664@yahoo.co.jp>
2022-06-11 18:12:53 +0900

    [Update]クリスタル完成

---
+++ Assets/Scripts/MainObject/CrystalSpowner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalSpowner : MonoBehaviour
+{
+    [Header("Crystal")]
+    [SerializeField, Tooltip("ƒNƒŠƒXƒ^ƒ‹")] Crystal _crystal;
+
+
+    ObjectPool<Crystal> _crystalPool1;
+    void Start()
+    {
+        _crystalPool1 = new ObjectPool<Crystal>(_crystal, this.gameObject.transform); ;
+    }
+
+    public void SpownCrystal(Transform tf)
+    {
+        var crystal = _crystalPool1.Use();
+        crystal.transform.position = tf.position;
+    }
+}
